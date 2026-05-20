5970f6e405a7705c1eea3c3812ab66f8e887f640
WasiqAfnan <wansari964@yahoo.com>
2026-05-20 20:57:26 +0530

    feat: Add payment verification controller and route

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -52,3 +52,63 @@ export const createOrderController = async (req, res) => {
     });
   }
 };
+
+export const verifyPaymentController = async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+      req.body;
+
+    // validation
+    if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature) {
+      return res.status(400).json({
+        success: false,
+        message: "All payment fields are required",
+      });
+    }
+
+    // Generate Signature
+    const generatedSignature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+        .update(
+            `${razorpay_order_id}|${razorpay_payment_id}`
+        )
+        .digest("hex");
+
+    // Compare generated signature with the one received from Razorpay
+    const isAuthentic = generatedSignature === razorpay_signature;
+
+    // handle invalid signature
+    if (!isAuthentic) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid payment signature",
+      });
+    }
+
+    /*
+        TODO:Store successful payment in DB
+    */
+    // Example:
+    // payment_id
+    // order_id
+    // payment status
+    // user info
+
+    return res.status(200).json({
+      success: true,
+      message: "Payment verified successfully",
+      data: {
+        razorpay_order_id,
+        razorpay_payment_id,
+      },
+    });
+  } catch (error) {
+    console.error("Verify Payment Error:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Payment verification failed",
+      error: error.message,
+    });
+  }
+};
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import {
   createOrderController,
+  verifyPaymentController,
 } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 // Create Razorpay Order
 router.post("/create-order", createOrderController);
+
+// Verify Payment Signature
+router.post("/verify-payment", verifyPaymentController);
 
 export default router;
