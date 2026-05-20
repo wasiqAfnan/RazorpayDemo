0197e8716f1f83ada5dec934372d22e90753f492
WasiqAfnan <wansari964@yahoo.com>
2026-05-20 20:06:48 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+/*
+ * NOTE:
+ * We are using express.json() globally for normal APIs.
+ *
+ * BUT:
+ * Razorpay webhook route will later require express.raw()
+ * because webhook signature verification depends on raw body.
+ *
+ */
+
+
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// Health Check Route
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        success: true,
+        message: "Payment Demo API is running",
+    });
+});
+
+// Routes
+app.use("/api/v1/payments", paymentRoutes);
+
+export default app;
